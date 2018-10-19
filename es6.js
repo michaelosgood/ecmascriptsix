8d510791a87449dd43574a95867547e42e19a232
Michael Osgood <michael.f.osgood@gmail.com>
2018-10-18 20:24:28 -0700

    added const

--- es6.js
+++ es6.js
@@ -4,5 +4,7 @@ let five = 5;
 console.log(five + five);
 // returns a value of 10
 
-// const is a variable that' value will alway persist
-
+// const is a variable that's value will alway persist
+const ten = 10;
+console.log(ten + 1);
+// returns a value of 11
