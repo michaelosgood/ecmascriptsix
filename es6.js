4291bd247c81f151a6b89f9713d78a417351eb25
Michael Osgood <michael.f.osgood@gmail.com>
2018-10-18 20:16:47 -0700

    added a javascript file

---
+++ es6.js
@@ -0,0 +1,8 @@
+// Variables
+// let is a variable (aka binding) that's value can be changed
+let five = 5;
+console.log(five + five);
+// returns a value of 10
+
+// const is a variable that' value will alway persist
+
