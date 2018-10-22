c828c00abf434fbb337c3a13f88d5842a4d68de5
Michael Osgood <michael.f.osgood@gmail.com>
2018-10-21 18:10:12 -0700

    added a function example

---
+++ function.js
@@ -0,0 +1,20 @@
+const makeNoise = function() {
+    console.log("Pling!");
+  };
+  
+  makeNoise();
+  // → Pling!
+  
+  const power = function(base, exponent) {
+    let result = 1;
+    for (let count = 0; count < exponent; count++) {
+      result *= base;
+    }
+    return result;
+  };
+  
+  console.log(power(2, 10));
+  // → 1024
+
+  console.log("console.log(power(3, 4));")
+  console.log(power(3, 4));
